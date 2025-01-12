55292488621b81fd7566c90af8043d6b2e51ac7a
kj <111208020+jpn254960@users.noreply.github.com>
2025-01-12 03:08:11 -0300

    dd
    dd

--- site/index.js
+++ site/index.js
@@ -1,14 +1,21 @@
 function abrir(){
     var barra=document.getElementById('barra')
     var verificador= barra.className
+    var display=document.getElementById('pagina').style.display
     if(verificador=='barra' || verificador=='barra_0'){
         barra.className='barra_1'
         barra.style.transform='translateX(0%)'
-        document.getElementById('bt_barra').style.color='white'   
+        document.getElementById('bt_barra').style.color='white'
+        setTimeout(function(){
+            document.getElementById('pagina').style.display='none'
+        },300)
     }
     else if (verificador=='barra_1'){
-            barra.className='barra_0'
-            barra.style.transform='translateX(100%)'
-        document.getElementById('bt_barra').style.color='black'   
+        barra.className='barra_0'
+        barra.style.transform='translateX(100%)'
+        document.getElementById('bt_barra').style.position='absulute'
+        document.getElementById('bt_barra').style.color='black'
+        document.getElementById('pagina').style.display='auto'
+        document.getElementById('pagina').style.display='' || 'block'
     }
 }
